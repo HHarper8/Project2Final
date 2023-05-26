9d9da0522721a617fa0b6f151e21211f790e6966
tankk <kyle.tan@student.unimelb.edu.au>
2023-05-26 18:02:15 +1000

    final commit

--- PacManInTheTorusVerse/src/PacManGame/GameActorFactory.java
+++ PacManInTheTorusVerse/src/PacManGame/GameActorFactory.java
@@ -9,17 +9,13 @@ import java.util.Properties;
 //958908 Kai Yao Tan (tankk@student.unimelb.edu.au)
 //1270676 Aryan Puri (aryanp@student.unimelb.edu.au)
 //1272792 Henry Harper (hsharper@student.unimelb.edu.au)
-public class GameActorFactory
-{
+public class GameActorFactory {
   private int nbHorzCells;
   private int nbVertCells;
 
-  private int[][] mazeArray;
+
   private Game game;
   private Properties properties;
-
-
-
 
 
   public GameActorFactory(Game game, Properties properties) {
@@ -29,6 +25,7 @@ public class GameActorFactory
     this.nbHorzCells = Game.getNbHCells();
 
   }
+
 
 
   /**
